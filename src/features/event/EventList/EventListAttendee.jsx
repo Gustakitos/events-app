dfa376c33ac8592c19513167ab800e908060ce36
Luis Gustavo <gustakitos@Gustavos-Macbook.local>
2019-04-04 02:11:31 -0300

    Event Form added w styles and initial states

--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -3,10 +3,11 @@ import { List, Image } from "semantic-ui-react";
 
 class EventListAttendee extends Component {
   render() {
+    const {attendee} = this.props
     return (
       <div>
         <List.Item>
-            <Image as='a' size='mini' circular src='https://randomuser.me/api/portraits/women/42.jpg'/>
+            <Image as='a' size='mini' circular src={attendee.photoURL}/>
         </List.Item>
       </div>
     )
